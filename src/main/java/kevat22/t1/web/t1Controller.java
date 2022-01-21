ca051188a76d700412bd5babf9813fbd5fdeaf3f
AnnePe <anne.pelin@koodata.fi>
2022-01-21 13:43:45 +0200

    Tehtävät 1 ja 2

---
+++ src/main/java/kevat22/t1/web/t1Controller.java
@@ -0,0 +1,30 @@
+package kevat22.t1.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class t1Controller {
+
+	@RequestMapping("index")
+	@ResponseBody
+	public String Hello() {
+		return "This is the main page";
+	}
+	
+	@RequestMapping("contact")
+	@ResponseBody
+	public String Hello2() {
+		return "this is the contactpage";
+	}
+	
+	@RequestMapping("hello")
+	@ResponseBody
+	public String Hello(@RequestParam(name="location", required=false, defaultValue="Moon" ) String paikka ,
+	@RequestParam(name="name", required=false, defaultValue="dear friend" ) String nimi )
+{
+		return "Welcome to the  "+ paikka + " "+ nimi +"!";
+	}
+}
